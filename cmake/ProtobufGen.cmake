f9ab6735e84ec2abef69e5f172813261051b3a28
Aleksey Timin <atimin@gmail.com>
2022-06-27 11:40:41 +0200

    fix build

--- cmake/ProtobufGen.cmake
+++ cmake/ProtobufGen.cmake
@@ -2,38 +2,38 @@ set(PROTO_SPEC_ROOT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/proto_specs)
 
 # Generate protoc files
 set(PROTOBUF_SPEC_FILES
-        ${PROTO_SPEC_ROOT_DIR}/drift_proto/alignment_service/aligned_package.proto
-        ${PROTO_SPEC_ROOT_DIR}/drift_proto/common/drift_package.proto
-        ${PROTO_SPEC_ROOT_DIR}/drift_proto/common/data_payload.proto
-        ${PROTO_SPEC_ROOT_DIR}/drift_proto/common/status_code.proto
-        ${PROTO_SPEC_ROOT_DIR}/drift_proto/meta/meta_info.proto
-        ${PROTO_SPEC_ROOT_DIR}/drift_proto/trigger_service/trigger_message.proto
-        ${PROTO_SPEC_ROOT_DIR}/drift_proto/trigger_service/interval_trigger_message.proto
+        ${PROTO_SPEC_ROOT_DIR}/drift_protocol/alignment_service/aligned_package.proto
+        ${PROTO_SPEC_ROOT_DIR}/drift_protocol/common/drift_package.proto
+        ${PROTO_SPEC_ROOT_DIR}/drift_protocol/common/data_payload.proto
+        ${PROTO_SPEC_ROOT_DIR}/drift_protocol/common/status_code.proto
+        ${PROTO_SPEC_ROOT_DIR}/drift_protocol/meta/meta_info.proto
+        ${PROTO_SPEC_ROOT_DIR}/drift_protocol/trigger_service/trigger_message.proto
+        ${PROTO_SPEC_ROOT_DIR}/drift_protocol/trigger_service/interval_trigger_message.proto
         )
 
 set(PROTOBUF_FILES
-        ${CMAKE_BINARY_DIR}/drift_proto/alignment_service/aligned_package.pb.cc
-        ${CMAKE_BINARY_DIR}/drift_proto/common/drift_package.pb.cc
-        ${CMAKE_BINARY_DIR}/drift_proto/common/data_payload.pb.cc
-        ${CMAKE_BINARY_DIR}/drift_proto/common/status_code.pb.cc
-        ${CMAKE_BINARY_DIR}/drift_proto/meta/meta_info.pb.cc
-        ${CMAKE_BINARY_DIR}/drift_proto/trigger_service/trigger_message.pb.cc
-        ${CMAKE_BINARY_DIR}/drift_proto/trigger_service/interval_trigger_message.pb.cc
+        ${CMAKE_BINARY_DIR}/drift_protocol/alignment_service/aligned_package.pb.cc
+        ${CMAKE_BINARY_DIR}/drift_protocol/common/drift_package.pb.cc
+        ${CMAKE_BINARY_DIR}/drift_protocol/common/data_payload.pb.cc
+        ${CMAKE_BINARY_DIR}/drift_protocol/common/status_code.pb.cc
+        ${CMAKE_BINARY_DIR}/drift_protocol/meta/meta_info.pb.cc
+        ${CMAKE_BINARY_DIR}/drift_protocol/trigger_service/trigger_message.pb.cc
+        ${CMAKE_BINARY_DIR}/drift_protocol/trigger_service/interval_trigger_message.pb.cc
         )
 
 
 add_custom_command(OUTPUT ${PROTOBUF_FILES}
         COMMAND ${Protobuf_PROTOC_EXECUTABLE} -I=${PROTO_SPEC_ROOT_DIR}/
-        --cpp_out=${CMAKE_BINARY_DIR} ${PROTO_SPEC_ROOT_DIR}/drift_proto/alignment_service/*
+        --cpp_out=${CMAKE_BINARY_DIR} ${PROTO_SPEC_ROOT_DIR}/drift_protocol/alignment_service/*
 
         COMMAND ${Protobuf_PROTOC_EXECUTABLE} -I=${PROTO_SPEC_ROOT_DIR}/
-        --cpp_out=${CMAKE_BINARY_DIR} ${PROTO_SPEC_ROOT_DIR}/drift_proto/common/*
+        --cpp_out=${CMAKE_BINARY_DIR} ${PROTO_SPEC_ROOT_DIR}/drift_protocol/common/*
 
         COMMAND ${Protobuf_PROTOC_EXECUTABLE} -I=${PROTO_SPEC_ROOT_DIR}/
-        --cpp_out=${CMAKE_BINARY_DIR} ${PROTO_SPEC_ROOT_DIR}/drift_proto/meta/*
+        --cpp_out=${CMAKE_BINARY_DIR} ${PROTO_SPEC_ROOT_DIR}/drift_protocol/meta/*
 
         COMMAND ${Protobuf_PROTOC_EXECUTABLE} -I=${PROTO_SPEC_ROOT_DIR}/
-        --cpp_out=${CMAKE_BINARY_DIR} ${PROTO_SPEC_ROOT_DIR}/drift_proto/trigger_service/*
+        --cpp_out=${CMAKE_BINARY_DIR} ${PROTO_SPEC_ROOT_DIR}/drift_protocol/trigger_service/*
 
         DEPENDS ${PROTOBUF_SPEC_FILES}
         )
