d111d9f8a0b4a28f37dfd2629f67fc3b72fc51fc
Alexey Timin <atimin@gmail.com>
2022-10-19 14:39:38 +0200

    add basic and image examples for python

---
+++ docs/py_examples/drift_package_example.md
@@ -0,0 +1,7 @@
+# Basic Drift Package Example
+
+This example shows how to serialize and parse a [Drift Package](/docs/api/common) in Python.
+
+```py title="python/examples/drift_package_example.py"
+--8<-- "../python/examples/drift_package_example.py:"
+```
---
+++ python/examples/drift_package_example.py
@@ -0,0 +1,38 @@
+from drift_protocol.common import DriftPackage, DataPayload, StatusCode
+
+from google.protobuf.timestamp_pb2 import Timestamp
+from google.protobuf.any_pb2 import Any
+
+if __name__ == "__main__":
+    pb_time = Timestamp()
+    pb_time.GetCurrentTime()
+
+    # Create a package and serialize it
+    original = DriftPackage()
+    original.id = pb_time.ToMilliseconds()
+    original.source_timestamp.CopyFrom(pb_time)
+    original.publish_timestamp.CopyFrom(pb_time)
+    original.status = StatusCode.GOOD
+
+    # Prepare payload
+    payload = DataPayload()
+    payload.data = b"some data to send"
+
+    # Load JPG image, decode, decompose with wavelet transformation and denoise
+    img.import_from_file()
+
+    msg = Any()
+    msg.Pack(payload)
+    original.data.append(msg)
+
+    # Serialize package to message
+    message = original.SerializeToString()
+
+    # Parse the package
+    new_pacakge = DriftPackage()
+    new_pacakge.ParseFromString(message)
+    print(f"Package ID={new_pacakge.id}")
+
+    payload = DataPayload()
+    new_pacakge.data[0].Unpack(payload)
+    print(f"Data: {payload.data}")
